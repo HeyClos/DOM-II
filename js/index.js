86bcb982f15fe43456a1f0f0a3d3f57191f083a9
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-04-16 16:32:44 -0700

    DOMII

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Your code goes here
+
+
+// mouseover
+//  keydown
+//  wheel
+//  drag / drop
+//  load
+//  focus
+//  resize
+//  scroll
+//  select
+//  dblclick
+
+const signBtn = document.querySelectorAll(".btn");
+
+// signBtn.forEach(button => {
+//     button.addEventListener ("click", () => {
+//         event.target.style.backgroundColor = 'blue';
+//     }
+// })
+
+
+const goCrazy = document.querySelectorAll(".container home .content-section .text-content p");
+goCrazy[0].addEventListener('mouseover', (event) =>{
+    event.target.style.backgroundColor = 'yellow';
+})
+
+const mainNav = document.querySelector(".main-navigation");
+mainNav.addEventListener("click", event => {
+  event.target.style.color = "red";
+});
+
+const dest = document.querySelector(".content-destination");
+dest.addEventListener("click", () => {
+  console.log(`I'm pressing the key:${event.key}`)
+});
+
+const mainNav = document.querySelector(".main-navigation");
+mainNav.addEventListener("click", event => {
+  event.target.style.color = "red";
+});
